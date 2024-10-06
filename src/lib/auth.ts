c13e6c12322be989be14053b1a0faf43a4edb10a
solopaw <adamjoelfraser@icloud.com>
2024-10-06 12:39:26 +1000

    modified:   src/lib/auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,5 @@
-import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
-
+import NextAuth from "next-auth"
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub({
-    clientId:'',
-    clientSecret:''
-  })]
-});
+  providers: [],
+})
